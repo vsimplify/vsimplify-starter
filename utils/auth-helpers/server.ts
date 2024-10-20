8af7b066c00cec0afb57adc8e54815d4c36fcf59
VsimplifyIT <karan@vsimplifyit.com>
2024-10-20 14:55:38 -0700

    removed typo causing error

--- utils/auth-helpers/server.ts
+++ utils/auth-helpers/server.ts
@@ -3,8 +3,8 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-import { getURL, getErrorRedirect, getStatusRedirect } from 'utils/helpers';
-import { getAuthTypes } from 'utils/auth-helpers/settings';
+import { getURL, getErrorRedirect, getStatusRedirect } from '@/utils/helpers';
+import { getAuthTypes } from '@/utils/auth-helpers/settings';
 
 function isValidEmail(email: string) {
   var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
