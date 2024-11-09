d9ad2291e7fb6b74253fb084eef5c2c2730f7b15
VsimplifyIT <karan@vsimplifyit.com>
2024-11-08 18:30:03 -0800

    added missing code

--- lib/utils.ts
+++ lib/utils.ts
@@ -25,3 +25,20 @@ export function formatPercentage(value: number): string {
     maximumFractionDigits: 1
   }).format(value);
 }
+
+export async function streamToString(stream: ReadableStream<Uint8Array>): Promise<string> {
+  const reader = stream.getReader();
+  const decoder = new TextDecoder("utf-8");
+  let result = '';
+
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) {
+      break;
+    }
+    result += decoder.decode(value);
+  }
+
+  reader.releaseLock();
+  return result;
+}
